a23e7a1bfe3f25ec3fde4e1af00a415bbec39518
dantedxwolf <teacher.martin@gmail.com>
2020-12-03 08:02:09 +0600

    nginx added

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          delete <code>src/App.js</code> and save to reload.
+          now, the container is fully working hello yo
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-       marianne time
+       annie time lll
         </a>
       </header>
     </div>
